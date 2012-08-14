6578c6e9f08497dceb6dffe4d85985845472d3e9
Andrea Barbadoro <andij.cr@gmail.com>
2012-08-14 10:43:48 +0200

    yo. up and running

--- src/main/java/com/famenu/qrcodeBinder/HelloWorld.java
+++ src/main/java/com/famenu/qrcodeBinder/HelloWorld.java
@@ -6,18 +6,17 @@ import javax.servlet.http.*;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
 
-@SuppressWarnings("serial")
 public class HelloWorld extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        resp.getWriter().print("Server alive at " + req.getLocalAddr() + "!");
+        resp.getWriter().print("Hello from Java!\n");
     }
 
     public static void main(String[] args) throws Exception{
         Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-    	ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         server.setHandler(context);
         context.addServlet(new ServletHolder(new HelloWorld()),"/*");
